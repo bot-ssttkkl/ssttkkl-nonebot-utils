0c3391659888c8dbec690366e18b7d2ab946c390
ssttkkl <huang.wen.long@hotmail.com>
2023-08-30 01:20:43 +0800

    发送reaction时需要等待一下，否则会撞exceed frequency limit

--- ssttkkl_nonebot_utils/platform/handling_reaction/kaiheila.py
+++ ssttkkl_nonebot_utils/platform/handling_reaction/kaiheila.py
@@ -1,22 +1,38 @@
+from asyncio import sleep, create_task
 from contextlib import asynccontextmanager
 
+from nonebot import logger
 from nonebot.adapters.kaiheila import Event, Bot
 from nonebot.adapters.kaiheila.event import PrivateMessageEvent, ChannelMessageEvent, MessageEvent
 from nonebot.exception import MatcherException
 
 
-async def add_reaction(bot: Bot, event: Event, emoji: str):
-    if isinstance(event, PrivateMessageEvent):
-        await bot.directMessage_addReaction(msg_id=event.msg_id, emoji=emoji)
-    elif isinstance(event, ChannelMessageEvent):
-        await bot.message_addReaction(msg_id=event.msg_id, emoji=emoji)
+def add_reaction(bot: Bot, event: Event, emoji: str, delay: float = 0):
+    async def _():
+        try:
+            await sleep(delay)
+            if isinstance(event, PrivateMessageEvent):
+                await bot.directMessage_addReaction(msg_id=event.msg_id, emoji=emoji)
+            elif isinstance(event, ChannelMessageEvent):
+                await bot.message_addReaction(msg_id=event.msg_id, emoji=emoji)
+        except BaseException as e:
+            logger.exception(e)
+
+    create_task(_())
 
 
-async def remove_reaction(bot: Bot, event: Event, emoji: str):
-    if isinstance(event, PrivateMessageEvent):
-        await bot.directMessage_deleteReaction(msg_id=event.msg_id, emoji=emoji, user_id=bot.self_id)
-    elif isinstance(event, ChannelMessageEvent):
-        await bot.message_deleteReaction(msg_id=event.msg_id, emoji=emoji, user_id=bot.self_id)
+def remove_reaction(bot: Bot, event: Event, emoji: str, delay: float = 0):
+    async def _():
+        try:
+            await sleep(delay)
+            if isinstance(event, PrivateMessageEvent):
+                await bot.directMessage_deleteReaction(msg_id=event.msg_id, emoji=emoji, user_id=bot.self_id)
+            elif isinstance(event, ChannelMessageEvent):
+                await bot.message_deleteReaction(msg_id=event.msg_id, emoji=emoji, user_id=bot.self_id)
+        except BaseException as e:
+            logger.exception(e)
+
+    create_task(_())
 
 
 @asynccontextmanager
@@ -24,13 +40,13 @@ async def handling_reaction(bot: Bot, event: Event):
     if not isinstance(event, MessageEvent):
         return
 
-    await add_reaction(bot, event, ":flushed_face:")  # 处理中：😳
+    add_reaction(bot, event, ":flushed_face:")  # 处理中：😳
     try:
         yield
-        await add_reaction(bot, event, ":face_blowing_a_kiss:")  # 处理完毕：😘
+        add_reaction(bot, event, ":face_blowing_a_kiss:", delay=1)  # 处理完毕：😘
     except BaseException as e:
         if not isinstance(e, MatcherException):
-            await add_reaction(bot, event, ":loudly_crying_face:")  # 处理出错：😭
+            add_reaction(bot, event, ":loudly_crying_face:", delay=1)  # 处理出错：😭
         raise e
     finally:
-        await remove_reaction(bot, event, ":flushed_face:")  # 处理中：😳
+        remove_reaction(bot, event, ":flushed_face:", delay=2)  # 处理中：😳
