7ad36926a8a9f857bc893228e0db7afd1bbfbc91
ssttkkl <huang.wen.long@hotmail.com>
2023-08-30 22:38:47 +0800

    添加更多东西

--- ssttkkl_nonebot_utils/platform/upload_file/onebot/v11.py
+++ ssttkkl_nonebot_utils/platform/upload_file/onebot/v11.py
@@ -1,14 +1,6 @@
-from nonebot import require
+from ssttkkl_nonebot_utils.requires import silently_requires
 
-from ssttkkl_nonebot_utils.nonebot import log_only_if_trace
-
-
-@log_only_if_trace()
-def _():
-    require("nonebot_plugin_gocqhttp_cross_machine_upload_file")
-
-
-_()
+silently_requires("nonebot_plugin_gocqhttp_cross_machine_upload_file")
 
 from nonebot_plugin_gocqhttp_cross_machine_upload_file import upload_file
 
