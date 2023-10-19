0243521affeca74ee2e3971e186cb8ad1251a098
ssttkkl <huang.wen.long@hotmail.com>
2023-10-19 22:07:04 +0800

    版本 0.1.18

--- ssttkkl_nonebot_utils/platform/get_user_nickname/kaiheila.py
+++ ssttkkl_nonebot_utils/platform/get_user_nickname/kaiheila.py
@@ -1,8 +1,9 @@
-from nonebot import get_bot
+from nonebot import get_bot, logger
 from nonebot.adapters.kaiheila import Bot
 from nonebot_plugin_session import Session, SessionIdType
 
 from .cache import cache
+from .fallback import get_user_nickname as fallback
 
 
 async def get_user_nickname(session: Session) -> str:
@@ -10,9 +11,13 @@ async def get_user_nickname(session: Session) -> str:
 
     res = cache.get(cache_key)
     if res is None:
-        bot: Bot = get_bot(session.bot_id)
-        view = await bot.user_view(user_id=session.id1, guild_id=session.id3)
-        res = view.nickname
+        try:
+            bot: Bot = get_bot(session.bot_id)
+            view = await bot.user_view(user_id=session.id1, guild_id=session.id3)
+            res = view.nickname
+        except BaseException as e:
+            logger.opt(exception=e).error("获取用户昵称时发生错误")
+            res = await fallback(session)
 
     cache[cache_key] = res
     return res
--- ssttkkl_nonebot_utils/platform/get_user_nickname/qqguild.py
+++ ssttkkl_nonebot_utils/platform/get_user_nickname/qqguild.py
@@ -1,8 +1,9 @@
-from nonebot import get_bot
+from nonebot import get_bot, logger
 from nonebot.adapters.qqguild import Bot
 from nonebot_plugin_session import Session, SessionIdType
 
 from .cache import cache
+from .fallback import get_user_nickname as fallback
 
 
 async def get_user_nickname(session: Session) -> str:
@@ -10,9 +11,13 @@ async def get_user_nickname(session: Session) -> str:
 
     res = cache.get(cache_key)
     if res is None:
-        bot: Bot = get_bot(session.bot_id)
-        member = await bot.get_member(guild_id=session.id3, user_id=session.id1)
-        res = member.nick
+        try:
+            bot: Bot = get_bot(session.bot_id)
+            member = await bot.get_member(guild_id=session.id3, user_id=session.id1)
+            res = member.nick
+        except BaseException as e:
+            logger.opt(exception=e).error("获取用户昵称时发生错误")
+            res = await fallback(session)
 
     cache[cache_key] = res
     return res
