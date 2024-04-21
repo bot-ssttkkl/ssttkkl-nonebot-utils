a8fe21c3f13a7e0b2d0de647ec1b16f74ea7688d
ssttkkl <huang.wen.long@hotmail.com>
2024-04-21 13:28:38 +0800

    替换部分实现

--- ssttkkl_nonebot_utils/platform/upload_file/onebot/v11.py
+++ ssttkkl_nonebot_utils/platform/upload_file/onebot/v11.py
@@ -1,14 +1,20 @@
 from __future__ import annotations
+
+import uuid
 from typing import TYPE_CHECKING
 
-from ....requires import silently_requires
+import aiofiles
+from nonebot import require
+
+require("nonebot_plugin_localstore")
+
+from nonebot_plugin_localstore import get_cache_file
 
 if TYPE_CHECKING:
     from os import PathLike
     from typing import Union, AsyncIterable, Iterator
 
-    from nonebot import Bot
-    from nonebot.internal.adapter import Event
+    from nonebot.adapters.onebot.v11 import Bot, Event, PrivateMessageEvent, GroupMessageEvent
 
 
 async def upload_file(bot: Bot, event: Event, filename: str,
@@ -16,10 +22,18 @@ async def upload_file(bot: Bot, event: Event, filename: str,
                       AsyncIterable[Union[str, bytes]],
                       Iterator[Union[str, bytes]]] = None,
                       path: Union[None, str, PathLike[str]] = None):
-    silently_requires("nonebot_plugin_gocqhttp_cross_machine_upload_file")
+    if path is None:
+        cache_file = get_cache_file(plugin_name="ssttkkl_nonebot_utils",
+                                    filename=str(uuid.uuid4()))
+        async with aiofiles.open(cache_file, "wb+") as buf:
+            await buf.write(data)
 
-    from nonebot_plugin_gocqhttp_cross_machine_upload_file import upload_file as do_update_file
-    return await do_update_file(bot, event, filename, data, path)
+        path = cache_file
+
+    if isinstance(event, PrivateMessageEvent):
+        bot.upload_private_file(user_id=event.user_id, file=path, name=filename)
+    elif isinstance(event, GroupMessageEvent):
+        bot.upload_group_file(group_id=event.group_id, file=path, name=filename)
 
 
 __all__ = ("upload_file",)
